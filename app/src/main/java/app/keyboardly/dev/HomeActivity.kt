88ed0bfbe637a2a57ded99dc85bddc6877563280
Zainal Fahrudin <fnzainal@gmail.com>
2023-01-18 19:31:11 +0700

    update, fixed navigation drawer

--- app/src/main/java/app/keyboardly/dev/HomeActivity.kt
+++ app/src/main/java/app/keyboardly/dev/HomeActivity.kt
@@ -6,20 +6,20 @@ import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.*
-import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
 import app.keyboardly.dev.databinding.ActivityHomeBinding
-import com.google.android.material.navigation.NavigationView
 import timber.log.Timber
 
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var mToggle: ActionBarDrawerToggle
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityHomeBinding
 
@@ -30,39 +30,68 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.appBarHome.toolbar)
 
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navController = findNavController(R.id.nav_host_fragment_content_home)
-//        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-
-        val navView: NavigationView = binding.navView
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_addon
-            ), drawerLayout
-        )
 
-        navView.setupWithNavController(navController)
-        navView.setNavigationItemSelectedListener { item ->
-            Timber.w("on click=> ${item.itemId}")
-            when (item.itemId) {
-                R.id.nav_home -> navController.navigate(R.id.nav_home)
-                R.id.nav_addon -> navController.navigate(R.id.nav_addon)
-                R.id.nav_blank -> navController.navigate(R.id.nav_blank)
-                else -> {
-                    Timber.w("unhandle - item = ${item.itemId}")
+        binding.apply {
+            val navController = findNavController(R.id.nav_host_fragment_content_home)
+            NavigationUI.setupActionBarWithNavController(this@HomeActivity, navController, drawerLayout)
+
+            appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.nav_home, R.id.nav_addon
+                ), drawerLayout
+            )
+
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            initDrawerIcon()
+
+            navView.setupWithNavController(navController)
+            navView.setNavigationItemSelectedListener { item ->
+                Timber.w("on click=> ${item.itemId}")
+                when (item.itemId) {
+                    R.id.nav_home -> navController.navigate(R.id.nav_home)
+                    R.id.nav_addon -> navController.navigate(R.id.nav_addon)
+                    R.id.nav_blank -> navController.navigate(R.id.nav_blank)
+                    else -> {
+                        Timber.w("unhandle - item = ${item.itemId}")
+                    }
                 }
+
+                drawerLayout.closeDrawer(GravityCompat.START)
+
+                true
             }
-
-            drawerLayout.closeDrawer(GravityCompat.START)
-
-            true
         }
+
     }
 
     private var isBack = false
+
+
+
+    private fun ActivityHomeBinding.initDrawerIcon() {
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            mToggle =
+                ActionBarDrawerToggle(
+                    this@HomeActivity, drawerLayout,
+                    R.string.open,
+                    R.string.close
+                )
+        }
+        /*mToggle.drawerArrowDrawable.color = ContextCompat.getColor(
+            this@MainActivity,
+            R.color.black
+        )*/
+//        mToggle.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+        mToggle.isDrawerIndicatorEnabled = true
+
+        drawerLayout.addDrawerListener(mToggle)
+        mToggle.syncState()
+    }
 
     override fun onBackPressed() {
         binding.apply {
@@ -87,6 +116,25 @@ class HomeActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.home, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_about->{
+                showAboutDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAboutDialog() {
+        val dialog = AlertDialog.Builder(this)
+            .setTitle(R.string.action_about)
+            .setMessage(R.string.text_about)
+            .setPositiveButton(android.R.string.ok){ d, _ ->
+                d.dismiss()
+            }
+        dialog.show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
