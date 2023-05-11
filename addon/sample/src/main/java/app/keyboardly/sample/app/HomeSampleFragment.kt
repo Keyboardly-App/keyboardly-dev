038395bfa35638b4359c3f173dca62567f9e30a7
Zainal Fahrudin <fnzainal@gmail.com>
2023-05-11 14:51:57 +0700

    update with view binding

--- addon/sample/src/main/java/app/keyboardly/sample/app/HomeSampleFragment.kt
+++ addon/sample/src/main/java/app/keyboardly/sample/app/HomeSampleFragment.kt
@@ -1,20 +1,23 @@
 package app.keyboardly.sample.app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import app.keyboardly.sample.R
+import androidx.fragment.app.Fragment
+import app.keyboardly.sample.databinding.FragmentHomeSampleBinding
 
 
 class HomeSampleFragment : Fragment() {
 
+    private var binding: FragmentHomeSampleBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentHomeSampleBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_sample, container, false)
+        return binding!!.root
     }
 }
